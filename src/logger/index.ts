7ffe45d8e5736194ffd95b74db7ba0d4d90fdc46
Isaac Miller <miller.isaac96@gmail.com>
2022-01-16 16:55:36 -0500

    fix bin and null logging config condition

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -10,16 +10,16 @@ export function getLogger(config: RuntimeConfig, source: string): Logger {
   if (file?.path) {
     return new FileLogger(
       file.path,
-      console.format ?? format ?? "json",
-      console.level ?? defaultLevel,
+      console?.format ?? format ?? "json",
+      console?.level ?? defaultLevel,
       file.level ?? defaultLevel,
       file.format ?? "json",
       source
     );
   } else {
     return new ConsoleLogger(
-      console.level ?? defaultLevel,
-      console.format ?? format ?? "text",
+      console?.level ?? defaultLevel,
+      console?.format ?? format ?? "text",
       source
     );
   }
